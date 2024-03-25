1939f3cb96cd01a609db5429fbdbbf0d55f43c37
HoussemZ4 <114112576+HoussemZ4@users.noreply.github.com>
2024-03-25 00:37:11 +0000

    python files and fixed nginx.cfg

---
+++ load_inventory.py
@@ -0,0 +1,20 @@
+import ansible_runner
+import sys
+
+# list all host names and IP addresses:
+out, err, rc = ansible_runner.run_command(
+    executable_cmd='ansible-inventory',
+    cmdline_args=['--list', '--yaml'],
+    input_fd=sys.stdin,
+    output_fd=sys.stdout,
+    error_fd=sys.stderr
+)
+
+# Ping all hosts and output results
+out, err, rc = ansible_runner.run_command(
+    executable_cmd='ansible',
+    cmdline_args=['all:localhost', '-m', 'ping'],
+    input_fd=sys.stdin,
+    output_fd=sys.stdout,
+    error_fd=sys.stderr
+)
---
+++ run_playbook.py
@@ -0,0 +1,11 @@
+import ansible_runner
+import requests
+
+r = ansible_runner.run(playbook='/workspaces/ensf400-lab5-ansible/hello.yml')
+
+response = requests.get('https://didactic-giggle-5j769rx59qqc4gj4-80.app.github.dev/')
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(f"Failed to fetch URL: {response.status_code} {response.reason}")
